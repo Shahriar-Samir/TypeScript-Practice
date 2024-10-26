8136677c38d72bc26710d6e9c6a759d7a3979374
Shahriar-Samir <shabusiness035@gmail.com>
2024-10-27 00:31:06 +0600

    add: day-1

---
+++ Day-1/src/index.ts
@@ -0,0 +1,16 @@
+// let sale= 32_4323_4
+// let course= 'level 2'
+// let enrolled = true
+// let level 
+
+// // in ts, not always have to anotate
+// // no value initialized will set to any type
+// // avoid using any type as much as possible
+
+// function render(doc:any){
+//     return doc
+// }
+
+// // array
+
+// let number:number[] = [1,2,3,4,'5']
